8f524e29f43ead1a640c84a886e52ac3e2f3a9ea
Dawood Sarfraz <dawoodsarfraz.cs@gmail.com>
2025-02-20 16:27:20 +0500

    upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from src.object_detection import object_detection
+
+def main():
+    print("Object Detection")
+    object_detection()
+    # object_tracking()
+
+
+if __name__ == "__main__":
+    main()
+
