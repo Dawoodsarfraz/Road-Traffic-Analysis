7fe3d524296703158dc100383a725aa0101294eb
Dawood Sarfraz <dawoodsarfraz.cs@gmail.com>
2025-02-25 18:04:20 +0500

    Update experiments.py

--- experiments.py
+++ experiments.py
@@ -7,7 +7,7 @@ def main():
     print("Starting Object Detection...")
 
     model_path = "Models/Yolov12/weights/yolov12x.pt"
-    media_source = "./media/videos/222.mp4"
+    input_media_source = "./media/videos/222.mp4"
     # media_source = "./media/images/image.png" # media_path
 
     # objects_to_detect = None  # Track all objects
@@ -17,13 +17,13 @@ def main():
 
     detector = ObjectDetector(model_path, conf_threshold, objects_to_detect)
 
-    if media_source.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tiff')):
-        frame = cv2.imread(media_source)
+    if input_media_source.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tiff')):
+        frame = cv2.imread(input_media_source)
         if frame is None:
-            print(f"Error: Could not open {media_source}")
+            print(f"Error: Could not open {input_media_source}")
             return
 
-        detected_objects = detector.process_image(media_source)
+        detected_objects = detector.process_image(input_media_source)
         annotated_frame = annotate_frame(frame, detected_objects)
 
         cv2.imshow("Object Detection", annotated_frame)
@@ -31,7 +31,7 @@ def main():
         cv2.destroyAllWindows()
 
     else:
-        for frame, detected_objects in detector.process_video(media_source):
+        for frame, detected_objects in detector.process_video(input_media_source):
             annotated_frame = annotate_frame(frame, detected_objects)  # Annotate in main.py
             cv2.imshow("Object Detection", annotated_frame)
 
